2188a7c94c9f70c71e8642cdaea5df7d422ddf92
[01;31m[K"name"[m[K: "Cate <[01;31m[K"email"[m[K: null>
2018-02-07 21:34:07 -0500

    Create routes for pages-to-be

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,5 +2,35 @@ get '/' do
   erb :index
 end
 
+get '/schedule' do
+  erb :index
+end
 
+get '/venues' do
+  erb :index
+end
+
+get '/music' do
+  erb :index
+end
+
+get '/instructors' do
+  erb :index
+end
+
+get '/workshops' do
+  erb :index
+end
+
+get '/register' do
+  erb :index
+end
+
+get '/travel' do
+  erb :index
+end
+
+get '/richmondfun' do
+  erb :index
+end
 
